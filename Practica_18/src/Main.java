3216a72a2de834d6d0a36e812e3c70e0d4862e08
alex <aleshabulanov@mail.ru>
2023-11-10 19:13:25 +0300

    Add eighteenth practical work

---
+++ Practica_18/src/Main.java
@@ -0,0 +1,19 @@
+import ru.mirea.it.ivbo.Exception1;
+import ru.mirea.it.ivbo.Exception2;
+import ru.mirea.it.ivbo.ThrowsDemo;
+import ru.mirea.it.ivbo.ThrowsDemo2;
+
+public class Main {
+    public static void main(String[] args) {
+//        Exception1 exception1 = new Exception1();
+//        exception1.exceptionDemo();
+
+//        Exception2 exception2 = new Exception2();
+//        exception2.exceptionDemo();
+
+//        ThrowsDemo throwsDemo = new ThrowsDemo();
+//        throwsDemo.printMessage(null);
+        ThrowsDemo2 throwsDemo2 = new ThrowsDemo2();
+        throwsDemo2.getKey();
+    }
+}
